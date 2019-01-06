9dac882ebb27ceb6decc6989eae121b2cb518360
sidemastergm <jersonmartinezsm@gmail.com>
2019-01-06 09:23:45 -0600

    Classes and inheritances

--- src/classes.js
+++ src/classes.js
@@ -1,9 +1,39 @@
 class User {
-    constructor(name, age){
+    constructor(name, age, email){
         this.name = name;
         this.age = age;
+        this.email = email;
+    }
+
+    showInfo(){
+        return `
+            <b>Name: </b>${this.name}<br/>
+            <b>Age: </b>${this.age}<br/>
+            <b>Email: </b>${this.email}<br/>
+            <hr/>
+        `;
     }
 }
 
-const Antonio = new User("Jerson Martínez", 22);
-document.write(`Your name is: ${Antonio.name} and you are ${Antonio.age} years old.`);
+class Student extends User {
+    constructor(name, age, email, career){
+        super(name, age, email);
+        this.career = career;
+    }
+
+    showInfo(){
+        return `
+            <b>Name: </b>${this.name}<br/>
+            <b>Age: </b>${this.age}<br/>
+            <b>Email: </b>${this.email}<br/>
+            <b>Career: </b>${this.career}<br/>
+            <hr/>
+        `;
+    }
+}
+
+const Instancia = new User("Jerson Martínez", 22, "jersonmartinezsm@gmail.com");
+document.write(Instancia.showInfo());
+
+const InstanciaTwo = new Student("Antonio Moreno", 22, "sidemastersm@gmail.com", "Telematic Engineering");
+document.write(InstanciaTwo.showInfo());
