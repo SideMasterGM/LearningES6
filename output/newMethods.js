3379e6f59225dee28d2bbbc96272ee89f9704f1c
sidemastergm <jersonmartinezsm@gmail.com>
2019-01-09 20:52:49 -0600

    Look for a letter

---
+++ output/newMethods.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var text = "Hello world";
+console.log(text, "start with h:", text.toLowerCase().startsWith('h'));
+console.log(text, "finish with d:", text.toLowerCase().endsWith('d'));
---
+++ src/newMethods.js
@@ -0,0 +1,4 @@
+const text = "Hello world";
+
+console.log(text, "start with h:", text.toLowerCase().startsWith('h'));
+console.log(text, "finish with d:", text.toLowerCase().endsWith('d'));
