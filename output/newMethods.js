8a0ee4dce770a0951743dc6322b136a18db81355
sidemastergm <jersonmartinezsm@gmail.com>
2019-01-09 21:04:07 -0600

    Find, Find Index with functions of type arrow

--- output/newMethods.js
+++ output/newMethods.js
@@ -3,3 +3,15 @@
 var text = "Hello world";
 console.log(text, "start with h:", text.toLowerCase().startsWith('h'));
 console.log(text, "finish with d:", text.toLowerCase().endsWith('d'));
+console.log(text, "include the word > world:", text.includes('world'));
+var friends = ["Jerson", "Antonio", "Martínez", "Moreno"];
+console.log("include the word > Martínez:", friends.includes("Martínez"));
+console.log("Searching:", friends.find(function (friend) {
+  return friend.length > 6;
+}));
+console.log("Searching:", friends.find(function (friend) {
+  return friend == "Jerson";
+}));
+console.log("Searching:", friends.findIndex(function (friend) {
+  return friend == "Moreno";
+}));
--- src/newMethods.js
+++ src/newMethods.js
@@ -2,3 +2,21 @@ const text = "Hello world";
 
 console.log(text, "start with h:", text.toLowerCase().startsWith('h'));
 console.log(text, "finish with d:", text.toLowerCase().endsWith('d'));
+
+console.log(text, "include the word > world:", text.includes('world'));
+
+const friends = ["Jerson", "Antonio", "Martínez", "Moreno"];
+
+console.log("include the word > Martínez:", friends.includes("Martínez"));
+
+console.log("Searching:", friends.find( (friend) => {
+    return friend.length > 6;
+}));
+
+console.log("Searching:", friends.find( (friend) => {
+    return friend == "Jerson";
+}));
+
+console.log("Searching:", friends.findIndex( (friend) => {
+    return friend == "Moreno";
+}));
