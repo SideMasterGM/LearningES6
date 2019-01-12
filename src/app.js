ca8e06904c362088c199f7568d147740928536e8
sidemastergm <jersonmartinezsm@gmail.com>
2019-01-11 18:21:38 -0600

    Exporting and importing functions

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import {alertMessage, greeting} from "./modules/alertMessage"
+
+alertMessage("Hello World!");
+
+greeting("I'm saying hello!");
---
+++ src/modules/alertMessage.js
@@ -0,0 +1,10 @@
+const alertMessage = (message) => {
+    alert(message);
+}
+
+const greeting = (message) => {
+    console.log(message);
+}
+
+export {alertMessage, greeting};
+// alertMessage("Hello world!");
